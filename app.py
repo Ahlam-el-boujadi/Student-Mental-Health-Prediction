a298966163c9821ad9e8f647fd1594289bfde15b
Ahlam-el-boujadi <elboujadiahlam@gmail.com>
2026-09-02 16:33:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    data = request.form
+
+    print(data)
+
+    return "Data received successfully"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
