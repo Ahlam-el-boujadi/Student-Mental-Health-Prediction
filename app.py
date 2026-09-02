4e59cdda06a52818752fec76246c8097520208e2
Ahlam-el-boujadi <elboujadiahlam@gmail.com>
2026-09-02 16:36:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,30 @@ def predict():
 
     data = request.form
 
-    print(data)
+    gender = data["gender"]
+    age = data["age"]
+    profession = data["profession"]
+    academic_pressure = data["academic_pressure"]
+    work_pressure = data["work_pressure"]
+    cgpa = data["cgpa"]
+    study_satisfaction = data["study_satisfaction"]
+    job_satisfaction = data["job_satisfaction"]
+    sleep_duration = data["sleep_duration"]
+    dietary_habits = data["dietary_habits"]
+    degree = data["degree"]
+    suicidal_thoughts = data["suicidal_thoughts"]
+    work_study_hours = data["work_study_hours"]
+    financial_stress = data["financial_stress"]
+    family_history = data["family_history"]
+
+    print("Gender :", gender)
+    print("Age :", age)
+    print("Profession :", profession)
+    print("CGPA :", cgpa)
 
     return "Data received successfully"
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
